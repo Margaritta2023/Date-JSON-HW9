0d0329f6e09489398d3e7ca2749515cf87b20148
Margaritta2023 <margarittamkn@gmail.com>
2023-09-29 18:47:57 +0400

    Format Date

---
+++ 4_formatDate.js
@@ -0,0 +1,22 @@
+function formatDate(date) {
+    let months = {
+        0: "January",
+        1: "February",
+        2: "March",
+        3: "April",
+        4: "May",
+        5: "June",
+        6: "July",
+        7: "August",
+        8: "September",
+        9: "October",
+        10: "November",
+        11: "December"
+    }
+ 
+
+    return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+}
+
+console.log(formatDate(new Date("2020-05-14")));
+
